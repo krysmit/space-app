c3140ca51e4af7e1b5741720c9c592721d725abf
Krystyn <krskrsmith@gmail.com>
2021-11-07 12:36:06 -0600

    not sure if all of the code in the bottom of hisRoutes is needed, but I wrote it in just in case for now. histRoutes generally set for now, just need to run and test it.

--- controllers/histRoutes.js
+++ controllers/histRoutes.js
@@ -26,3 +26,32 @@ router.get('/searchhistory', async (req, res) => {
 }
 });
 
+
+//middleware
+router.get('/searchhistory', withAuth, async (req, res) => {
+    try {
+        const userData = await  Project.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Project }],
+        });
+        const user = userData.get({ plain: true});
+
+        res.render('project', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//if already logged in, redirect the request
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/project');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;

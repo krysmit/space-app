b693ecca11dd8c5502c7c8d664f03399ce9ab881
Krystyn <krskrsmith@gmail.com>
2021-11-07 12:27:37 -0600

    more editing in histRoutes to make search history appear on the page

--- controllers/histRoutes.js
+++ controllers/histRoutes.js
@@ -1,17 +1,28 @@
 const router = require('express').Router();
+const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/searchhistory', async (req, res) => {
-    //const results = searchresults
-
     try {
-        // Get all search history results    
+        // Get all search history results 
+        const projectData = await Project.findAll({
+            include: [
+                {
+                model: Project,
+                    attributes: ['city'],
+                },
+            ],
+        });
+
+        const projects = projectData.map((project) => project.get({ plain: true }));
 
     res.render('searchhistory', {
         //results,
         logged_in: req.session.logged_in
     });
+
 } catch (err) {
     res.status(500).json(err);
 }
 });
+

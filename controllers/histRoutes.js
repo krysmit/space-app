dbec80795c4823ba3da311378df51a9766677be0
Krystyn <krskrsmith@gmail.com>
2021-11-07 12:20:50 -0600

    partial psudocode for search history results based off of db once that is built

--- controllers/histRoutes.js
+++ controllers/histRoutes.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
+router.get('/searchhistory', async (req, res) => {
+    //const results = searchresults
+
+    try {
+        // Get all search history results    
+
+    res.render('searchhistory', {
+        //results,
+        logged_in: req.session.logged_in
+    });
+} catch (err) {
+    res.status(500).json(err);
+}
+});

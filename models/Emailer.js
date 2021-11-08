b22acf6ecc0271f74463b25c8407e57a5503e19d
Krystyn Smith <krskrsmith@gmail.com>
2021-11-08 17:04:57 -0600

    edited the code for the emailer model slightly before getting the database in heroku set up for it

--- models/Emailer.js
+++ models/Emailer.js
@@ -3,3 +3,24 @@ const sequelize = require('../config/connection');
 
 class Emailer extends Model {}
 
+Emailer.init(
+    {
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'emailer',
+      }
+);
+
+module.exports = Emailer;

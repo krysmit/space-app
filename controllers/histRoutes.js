c574f0821deb4e4ef9c975bd1115e568514f39f9
Krystyn Smith <krskrsmith@gmail.com>
2021-11-09 16:34:45 -0600

    deleted some commented out code. edited the seeds.js file to reflect the addition of the emailer seed

--- controllers/histRoutes.js
+++ controllers/histRoutes.js
@@ -2,32 +2,7 @@ const router = require('express').Router();
 const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/searchhistory', async (req, res) => {
-//     try {
-//         // Get all search history results 
-//         const projectData = await Project.findAll({
-//             include: [
-//                 {
-//                 model: Project,
-//                     attributes: ['city'],
-//                 },
-//             ],
-//         });
 
-//         const projects = projectData.map((project) => project.get({ plain: true }));
-
-//     res.render('searchhistory', {
-//         //results,
-//         logged_in: req.session.logged_in
-//     });
-
-// } catch (err) {
-//     res.status(500).json(err);
-// }
-// });
-
-
-//middleware
 router.get('/searchhistory', withAuth, async (req, res) => {
     try {
         const userData = await  User.findByPk(req.session.user_id, {

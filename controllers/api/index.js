fa322931c88f5fb33ad4899d2f581824216f0170
Krystyn Smith <krskrsmith@gmail.com>
2021-11-09 12:49:32 -0600

    got the mailer functioning completely

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const mailer = require('./mailer');
 // const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
 // router.use('/projects', projectRoutes);
+router.use('/mail', mailer);
 
 module.exports = router;
